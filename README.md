67699139bd3ca10aec22143d9c6bc4b10814b432
Jasem Qabazard <jasemqabazard@yahoo.com>
2018-04-14 08:48:05 +0300

    add-bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # zario
+============server-setup======git branch============
+   - added the express server, mongodb, and ngzario built by ng new for angular
+==========add-bootsrap4=======git branch============
+   - remove all test files from angular 
+   - create .gitignore file to ignore all node_modules
+   - add bootsrap and its dependencies to angular
+   - add server.log file and console.log for server activities 
+   - change the angular build to produce public folder on server 
+      level with all trhe directions to the new index.js file
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 const path = require('path');
+const fs=require('fs');
+
+const app = express();
+app.use(bodyParser.json());
+const port = process.env.port || 8080;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
@@ -13,12 +18,25 @@ mongoose.connect(config.uri, (err) => {
    }
 });
 
-app.use(express.static(__dirname + '/ng-zario/dist/'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname +'/ng-zarion/dist/index.html'));
+app.use((req, res, next) => {
+   var now = new Date().toString();
+   var log = `Server Log at: ${now}: ${req.method} ${req.url}`;
+
+   console.log(log);
+   fs.appendFile('server.log', log + '\n', (err) => {
+      if (err) {
+         console.log('Unable to write to server.log');
+      }
+   });
+   next();
 });
 
-app.listen(8080, () => {
-   console.log('Starting Server at Port 8080');
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname +'/public/index.html'));
+});
+
+app.listen(port, () => {
+   console.log('Starting Server at Port ' + port);
 });
