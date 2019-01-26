35a4a08af0525e96ad7b50275098d1a0f209e036
Jasem M H Qsbazard <jasemqabazard@yahoo.com>
2019-01-26 08:53:10 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto').randomBytes(256).toString('hex');
-
-module.exports = {
-   'dburi': 'mongodb://localhost:27017/zario',
-   // secret: crypto,
-   'secretKey':'12398745630218907654',
-   'db': 'zario'
-}
